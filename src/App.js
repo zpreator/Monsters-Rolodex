06ff4d93a263291e94f6c014fded457901524c2d
zach preator <zachary.preator@gmail.com>
2020-07-19 23:05:31 -0600

    test edit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h4>This is a header</h4>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
